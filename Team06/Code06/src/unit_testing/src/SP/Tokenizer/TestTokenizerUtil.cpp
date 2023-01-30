f9301c8e74f2bc9465c9cf8a245d8fd50fc74505
Ryan Cheung <ryan9cheung@gmail.com>
2023-01-30 22:12:18 +0800

    Refactor tokenizer util tests

--- Team06/Code06/src/unit_testing/src/SP/Tokenizer/TestTokenizerUtil.cpp
+++ Team06/Code06/src/unit_testing/src/SP/Tokenizer/TestTokenizerUtil.cpp
@@ -1,58 +1,62 @@
 #include "catch.hpp"
 #include "../../../../spa/src/SP/Tokenizer.h"
 
-TEST_CASE("Checks that an integer number is in the string") {
+TEST_CASE("Valid Integer") {
 
 	Tokenizer tokenizer;
-	std::string testString = "23";
-	REQUIRE(tokenizer.isInteger(testString));
+
+	SECTION("Checks that an integer number is in the string") {
+		std::string testString = "23";
+		REQUIRE(tokenizer.isInteger(testString));
+	}
 }
 
-TEST_CASE("Checks that leading zero is not a valid integer") {
+TEST_CASE("Invalid Integer") {
 
 	Tokenizer tokenizer;
-	std::string testString = "023";
-	REQUIRE(!tokenizer.isInteger(testString));
+
+	SECTION("Checks that leading zero is not a valid integer") {
+		std::string testString = "023";
+		REQUIRE(!tokenizer.isInteger(testString));
+	}
+
+	SECTION("Checks that a mix of alphanumeric is not a valid integer") {
+		std::string testString = "1test2";
+		REQUIRE(!tokenizer.isInteger(testString));
+	}
+
+	SECTION("Checks that a mix of symbols is not a valid integer") {
+		std::string testString = "23*4";
+		REQUIRE(!tokenizer.isInteger(testString));
+	}
 }
 
-TEST_CASE("Checks that a mix of alphanumeric is not a valid integer") {
+TEST_CASE("Valid String") {
 
 	Tokenizer tokenizer;
-	std::string testString = "1test2";
-	REQUIRE(!tokenizer.isInteger(testString));
+
+	SECTION("Checks for a valid name with alphabets") {
+		std::string testString = "tEsT";
+		REQUIRE(tokenizer.isName(testString));
+	}
+
+	SECTION("Checks that a mix of alphanumeric is a valid name") {
+		std::string testString = "t0e1s2t3";
+		REQUIRE(tokenizer.isName(testString));
+	}
 }
 
-TEST_CASE("Checks that a mix of symbols is not a valid integer") {
+TEST_CASE("Invalid String") {
 
 	Tokenizer tokenizer;
-	std::string testString = "23*4";
-	REQUIRE(!tokenizer.isInteger(testString));
-}
 
-TEST_CASE("Checks for a valid name with alphabets") {
+	SECTION("Checks that a valid name has to start with an alphabet") {
+		std::string testString = "1abc";
+		REQUIRE(!tokenizer.isName(testString));
+	}
 
-	Tokenizer tokenizer;
-	std::string testString = "tEsT";
-	REQUIRE(tokenizer.isName(testString));
-}
-
-TEST_CASE("Checks that a mix of alphanumeric is a valid name") {
-
-	Tokenizer tokenizer;
-	std::string testString = "t0e1s2t3";
-	REQUIRE(tokenizer.isName(testString));
-}
-
-TEST_CASE("Checks that a valid name has to start with an alphabet") {
-
-	Tokenizer tokenizer;
-	std::string testString = "1abc";
-	REQUIRE(!tokenizer.isName(testString));
-}
-
-TEST_CASE("Checks that a valid name must not contain symbols") {
-
-	Tokenizer tokenizer;
-	std::string testString = "abc&de";
-	REQUIRE(!tokenizer.isName(testString));
+	SECTION("Checks that a valid name must not contain symbols") {
+		std::string testString = "abc&de";
+		REQUIRE(!tokenizer.isName(testString));
+	}
 }
