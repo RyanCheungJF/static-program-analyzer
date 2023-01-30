a141939d811f1e9d4791c6d12dbac89f06aca2c8
Darren <darrenhoon.ad@gmail.com>
2023-01-30 08:49:20 +0800

    add write and read logic for FollowsT

--- Team06/Code06/src/spa/src/PKB/storage/FollowsTStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/FollowsTStorage.cpp
@@ -1,9 +1,32 @@
 #include "FollowsTStorage.h"
 
-void FollowsTStorage::writeFollowsT(StmtNum followee, StmtNum follower) {}
+void FollowsTStorage::writeFollowsT(StmtNum followee, StmtNum follower) {
+    followee_followers[followee].insert(follower);
+    follower_followees[follower].insert(followee);
+    return;
+}
 
-void FollowsTStorage::writeFollowsT(StmtNum followee, std::vector<StmtNum> followers) {}
+void FollowsTStorage::writeFollowsT(StmtNum followee, std::vector<StmtNum> followers) {
+    for (StmtNum follower: followers) {
+        followee_followers[followee].insert(follower);
+        follower_followees[follower].insert(followee);
+    }
+    return;
+}
 
-std::unordered_set<StmtNum> FollowsTStorage::getFollowers(StmtNum followee) {}
+std::unordered_set<StmtNum> FollowsTStorage::getFollowers(StmtNum followee) {
+    if (followee_followers.find(followee) == followee_followers.end()) {
+        std::unordered_set<StmtNum> emptySet;
+        return emptySet;
+    }
+    return followee_followers[followee];
+}
 
-std::unordered_set<StmtNum> FollowsTStorage::getFollowees(StmtNum follower) {}
+std::unordered_set<StmtNum> FollowsTStorage::getFollowees(StmtNum follower) {
+    if (follower_followees.find(follower) == follower_followees.end()) {
+        std::unordered_set<StmtNum> emptySet;
+        return emptySet;
+    }
+    return follower_followees[follower];
+
+}
