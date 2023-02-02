d2f92073bbf64f02474aeaaaf41522150d001526
calvintanwj <calvintanwj127@gmail.com>
2023-02-02 22:32:03 +0800

    Add template code for unit testing of parser

---
+++ Team06/Code06/src/unit_testing/src/SP/Parser/TestParser.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+#include "../SP/Tokenizer.h"
+#include "../SP/Parser.h"
+#include "../SP/SPExceptions.h"
+
+TEST_CASE("Valid Source Program") {
+
+}
+
+TEST_CASE("Invalid Source Program") {
+
+}
