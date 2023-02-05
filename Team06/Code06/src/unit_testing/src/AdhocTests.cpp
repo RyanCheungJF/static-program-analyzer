b48d6030815c8d8f98aaf9cd473a2f5a6a4ca264
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-02-05 14:01:17 +0800

    small fix for test case

--- Team06/Code06/src/unit_testing/src/AdhocTests.cpp
+++ Team06/Code06/src/unit_testing/src/AdhocTests.cpp
@@ -8,38 +8,7 @@
 using namespace std;
 
 TEST_CASE("adhoc") {
-    string sampleProgram = "procedure Example {\n"
-                           "  x = 2;\n"
-                           "  z = 3;\n"
-                           "  i = 5;\n"
-                           "  while (i!=0) {\n"
-                           "    x = x - 1;\n"
-                           "    if (x==1) then {\n"
-                           "      z = x + 1; }\n"
-                           "    else {\n"
-                           "      y = z + x; }\n"
-                           "    z = z + x + i;\n"
-                           "    call q;\n"
-                           "    i = i - 1; }\n"
-                           "  call p; }\n"
-                           "\n"
-                           "procedure p {\n"
-                           "  if (x<0) then {\n"
-                           "    while (i>0) {\n"
-                           "      x = z * 3 + 2 * y;\n"
-                           "      call q;\n"
-                           "      i = i - 1; }\n"
-                           "    x = x + 1;\n"
-                           "    z = x + z; }\n"
-                           "  else {\n"
-                           "    z = 1; }\n"
-                           "  z = z + x + i; }\n"
-                           "\n"
-                           "procedure q {\n"
-                           "  if (x==1) then {\n"
-                           "    z = x + 1; }\n"
-                           "  else {\n"
-                           "    x = z + x; } }";
+    string sampleProgram = "/Users/faruq/Desktop/Books/Y3-S2/CS3203/team_project/22s2-cp-spa-team-06/Team06/Code06/tests/Sample_source.txt";
     SP sourceProcessor;
     WritePKB writePKB;
     ReadPKB readPKB;
