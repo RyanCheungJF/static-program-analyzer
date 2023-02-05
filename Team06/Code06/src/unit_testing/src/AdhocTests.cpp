2cf3468cd70cae9e9b9f3285481510f20bf6e3ef
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-02-05 13:57:21 +0800

    updated working adhoc test

--- Team06/Code06/src/unit_testing/src/AdhocTests.cpp
+++ Team06/Code06/src/unit_testing/src/AdhocTests.cpp
@@ -3,26 +3,72 @@
 #include "PKB/PKB.h"
 #include "PKB/ReadPKB.h"
 #include "PKB/WritePKB.h"
+#include "SP.h"
 #include "qps/QPS.h"
 using namespace std;
 
 TEST_CASE("adhoc") {
-	QPS qps;
-    WritePKB writePkb;
-    ReadPKB readPkb;
+    string sampleProgram = "procedure Example {\n"
+                           "  x = 2;\n"
+                           "  z = 3;\n"
+                           "  i = 5;\n"
+                           "  while (i!=0) {\n"
+                           "    x = x - 1;\n"
+                           "    if (x==1) then {\n"
+                           "      z = x + 1; }\n"
+                           "    else {\n"
+                           "      y = z + x; }\n"
+                           "    z = z + x + i;\n"
+                           "    call q;\n"
+                           "    i = i - 1; }\n"
+                           "  call p; }\n"
+                           "\n"
+                           "procedure p {\n"
+                           "  if (x<0) then {\n"
+                           "    while (i>0) {\n"
+                           "      x = z * 3 + 2 * y;\n"
+                           "      call q;\n"
+                           "      i = i - 1; }\n"
+                           "    x = x + 1;\n"
+                           "    z = x + z; }\n"
+                           "  else {\n"
+                           "    z = 1; }\n"
+                           "  z = z + x + i; }\n"
+                           "\n"
+                           "procedure q {\n"
+                           "  if (x==1) then {\n"
+                           "    z = x + 1; }\n"
+                           "  else {\n"
+                           "    x = z + x; } }";
+    SP sourceProcessor;
+    WritePKB writePKB;
+    ReadPKB readPKB;
     PKB pkb;
     FollowsStorage fs;
-    pkb.followsStorage = &fs;
+    EntityStorage et;
+    ProcedureStorage ps;
     StmtStorage sts;
+    QPS qps;
+    pkb.entityStorage = &et;
+    pkb.procedureStorage = &ps;
     pkb.statementStorage = &sts;
-    writePkb.setInstancePKB(pkb);
-    readPkb.setInstancePKB(pkb);
+    pkb.followsStorage = &fs;
+    writePKB.setInstancePKB(pkb);
+    readPKB.setInstancePKB(pkb);
+//    QPS qps;
+//    WritePKB writePkb;
+//    ReadPKB readPkb;
+//    PKB pkb;
+//    FollowsStorage fs;
+//    SP sourceProcessor;
+//    pkb.followsStorage = &fs;
+//    StmtStorage sts;
+    sourceProcessor.processFile(sampleProgram, &writePKB);
+//    writePkb.setFollows(1, 2);
 
-    writePkb.setFollows(1, 2);
-
-    string input = "stmt s; Select s such that Follows(1, s)";
+    string input = "stmt ifs; Select ifs such that Follows(1, ifs)";
     vector<string> expected{ "2" };
-    vector<string> res = qps.processQueries(input, readPkb);
+    vector<string> res = qps.processQueries(input, readPKB);
     for (string r : res) {
         cout << r << "\n";
     }
