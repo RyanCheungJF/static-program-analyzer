eabb31b6b12686f4897d5580bfbebc7f88b85685
Darren <darrenhoon.ad@gmail.com>
2023-02-06 10:27:24 +0800

    issue with non-complete expressions being passed into parseExpression

--- Team06/Code06/src/unit_testing/src/PKB/TestPattern.cpp
+++ Team06/Code06/src/unit_testing/src/PKB/TestPattern.cpp
@@ -67,12 +67,18 @@ TEST_CASE("Support for pattern query of type pattern(\"a\", _\"v\"_)") {
     qs2.lhs = "z";
     qs2.pattern = "_5_";
 
+
+    QueryStub qs3;
+    qs3.lhs = "z";
+    qs3.pattern = "_*_"; // in general, this is also failing for non-complete terms eg "* 5", "+ b", "/ c"
+
     std::vector<StmtNum> lines_qs1 = readPkb.interpretQuery(qs1);
     std::vector<StmtNum> lines_qs2 = readPkb.interpretQuery(qs2);
 
     bool res = true;
     res = res && lines_qs1.size() == 1;
     res = res && lines_qs2.size() == 1;
+    res = res && readPkb.interpretQuery(qs3).size() == 1;
 
     std::cout << "COMPLETE";
     REQUIRE(res);
@@ -136,12 +142,18 @@ TEST_CASE("Support for pattern query of type pattern(_, \"v\")") {
     qs2.lhs = "_";
     qs2.pattern = "z * 5";
 
+
+    QueryStub qs3;
+    qs3.lhs = "_";
+    qs3.pattern = "z";
+
     std::vector<StmtNum> lines_qs1 = readPkb.interpretQuery(qs1);
     std::vector<StmtNum> lines_qs2 = readPkb.interpretQuery(qs2);
 
     bool res = true;
     res = res && lines_qs1.size() == 1;
     res = res && lines_qs2.size() == 1;
+    res = res && readPkb.interpretQuery(qs3).size() == 0;
 
     std::cout << "COMPLETE";
     REQUIRE(res);
@@ -162,9 +174,9 @@ TEST_CASE("Support for pattern query of type pattern(_, _\"v\"_)") {
     std::string lhs = "z";
     std::unique_ptr<Expression> line1rhs = writePkb.buildSubtree("a + b / c");
     std::unique_ptr<Expression> line2rhs = writePkb.buildSubtree("z * 5");
-    std::unique_ptr<Expression> line3rhs = writePkb.buildSubtree("x * 10");
+    std::unique_ptr<Expression> line3rhs = writePkb.buildSubtree("x * 5");
 
-//    writePkb.writePattern(lhs, 1, std::move(line1rhs));
+    writePkb.writePattern(lhs, 1, std::move(line1rhs));
     writePkb.writePattern(lhs, 2, std::move(line2rhs));
     writePkb.writePattern(lhs, 3, std::move(line3rhs));
 
@@ -174,14 +186,14 @@ TEST_CASE("Support for pattern query of type pattern(_, _\"v\"_)") {
 
     QueryStub qs2;
     qs2.lhs = "_";
-    qs2.pattern = "_*_";
+    qs2.pattern = "_ * _"; // in general, this is also failing for non-complete terms eg "* 5", "+ b", "/ c"
 
-//    std::vector<StmtNum> lines_qs1 = readPkb.interpretQuery(qs1);
-    std::vector<StmtNum> lines_qs2 = readPkb.interpretQuery(qs2);
+    std::vector<StmtNum> lines_qs1 = readPkb.interpretQuery(qs1);
+//    std::vector<StmtNum> lines_qs2 = readPkb.interpretQuery(qs2);
 
     bool res = true;
-//    res = res && lines_qs1.size() == 1;
-    res = res && lines_qs2.size() == 2;
+    res = res && lines_qs1.size() == 1;
+//    res = res && lines_qs2.size() == 2;
 
     std::cout << "COMPLETE";
     REQUIRE(res);
