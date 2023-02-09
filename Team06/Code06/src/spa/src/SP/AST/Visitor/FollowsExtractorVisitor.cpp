01655434ff25970f19b14b768ee8ca227d837bcd
Ryan Cheung <ryan9cheung@gmail.com>
2023-02-09 15:52:01 +0800

    Add followsT logic

--- Team06/Code06/src/spa/src/SP/AST/Visitor/FollowsExtractorVisitor.cpp
+++ Team06/Code06/src/spa/src/SP/AST/Visitor/FollowsExtractorVisitor.cpp
@@ -8,13 +8,19 @@ void FollowsExtractorVisitor::visitProgram(Program *program) {}
 void FollowsExtractorVisitor::visitProcedure(Procedure* procedure) {}
 
 void FollowsExtractorVisitor::visitStatementList(StatementList *statementList) {
-	std::vector<std::pair<int, int>> followsVector;
+	// for follows
 	for (int i = 0; i < statementList->statements.size() - 1; i++) {
-		//auto followsPair = std::make_pair(statementList->statements[i]->statementNumber, statementList->statements[i + 1]->statementNumber);
-		//followsVector.push_back(followsPair);
 		writeApi->setFollows(statementList->statements[i]->statementNumber, statementList->statements[i + 1]->statementNumber);
 	}
-	//writeApi->setFollowsT(followsVector);
+	// for follows transitive
+	std::vector<std::pair<int, int>> followsTransitiveVector;
+	for (int i = 0; i < statementList->statements.size(); i++) {
+		for (int j = i + 1; j < statementList->statements.size(); j++) {
+			auto followsTransitivePair = std::make_pair(statementList->statements[i]->statementNumber, statementList->statements[j]->statementNumber);
+			followsTransitiveVector.push_back(followsTransitivePair);
+		}
+	}
+	writeApi->setFollowsT(followsTransitiveVector);
 }
 
 void FollowsExtractorVisitor::visitReadStatement(ReadStatement *readStatement) {}
