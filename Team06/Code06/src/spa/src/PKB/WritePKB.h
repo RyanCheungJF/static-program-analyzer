88566ca6ede17fee632acfd0d070d1506babe82d
Moley456 <kingstarwei10@gmail.com>
2023-02-11 14:51:12 +0800

    add method to write to ParentTStorage from WritePKB

--- Team06/Code06/src/spa/src/PKB/WritePKB.h
+++ Team06/Code06/src/spa/src/PKB/WritePKB.h
@@ -13,14 +13,14 @@ public:
     // Sets Follows relation in PKB
     void setFollows(StmtNum left, StmtNum right);
 
-
     // Sets FollowsT relation in PKB
     void setFollowsT(std::vector<std::pair<StmtNum, StmtNum>> followee_follower);
-
 
     // Sets Parent relation in PKB
     void setParent(StmtNum parent, StmtNum child);
 
+    // Sets ParentT relation in PKB
+    void setParentT(std::vector<std::pair<StmtNum, StmtNum>> parentChildrPairs);
 
     // Sets the statement along with the statement lines that they appear in
     void setStatement(Stmt s, std::vector<StmtNum> lines);
