0f1a3bab628df41e2fe98c5466feb308fb7fe72d
Darren <darrenhoon.ad@gmail.com>
2023-02-11 16:43:46 +0800

    add test cases UsesStorage

--- Team06/Code06/src/spa/src/PKB/storage/UsesStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/UsesStorage.cpp
@@ -2,15 +2,27 @@
 
 void UsesStorage::writeUsesProcedure(ProcedureName name, std::vector<Ent> entities) {
     procName_ent[name].insert(entities.begin(), entities.end());
+    return;
 }
 
 void UsesStorage::writeUsesStmtnum(StmtNum num, std::vector<Ent> entities) {
     stmtNum_ent[num].insert(entities.begin(), entities.end());
+    return;
 }
 
-std::unordered_set<Ent> UsesStorage::getUsesProcedureEntities(ProcedureName name) {
+std::unordered_set<Ent> UsesStorage::getUsesProcedure(ProcedureName name) {
+    // source code does not even contain this procedure
+    if (procName_ent.find(name) == procName_ent.end()) {
+        std::unordered_set<Ent> emptySet;
+        return emptySet;
+    }
     return procName_ent[name];
 }
-std::unordered_set<Ent> UsesStorage::getUsesStmtnumEntities(StmtNum num) {
+std::unordered_set<Ent> UsesStorage::getUsesStmtnum(StmtNum num) {
+    // source code does not even contain this statement number
+    if (stmtNum_ent.find(num) == stmtNum_ent.end()) {
+        std::unordered_set<Ent> emptySet;
+        return emptySet;
+    }
     return stmtNum_ent[num];
 }
--- Team06/Code06/src/spa/src/PKB/storage/UsesStorage.h
+++ Team06/Code06/src/spa/src/PKB/storage/UsesStorage.h
@@ -16,8 +16,8 @@ public:
     virtual void writeUsesProcedure(ProcedureName name, std::vector<Ent> entities);
     virtual void writeUsesStmtnum(StmtNum num, std::vector<Ent> entities);
 
-    virtual std::unordered_set<Ent> getUsesProcedureEntities(ProcedureName name);
-    virtual std::unordered_set<Ent> getUsesStmtnumEntities(StmtNum num);
+    virtual std::unordered_set<Ent> getUsesProcedure(ProcedureName name);
+    virtual std::unordered_set<Ent> getUsesStmtnum(StmtNum num);
 
 private:
     std::unordered_map<ProcedureName, std::unordered_set<Ent>> procName_ent;
