cd6e03070dd7a46992f3f280f4847af48c52b612
Darren <darrenhoon.ad@gmail.com>
2023-02-12 16:58:24 +0800

    delete container stoage table

--- Team06/Code06/src/spa/src/PKB/storage/ContainerStorage.cpp
+++
@@ -1,7 +0,0 @@
-#include "ContainerStorage.h"
-
-void ContainerStorage::writeContainerBlock(StmtNum num, StmtNum start, StmtNum end, std::vector<StmtNum> directly_nested_containerStmt) {
-    stmtNum_range_nestedContainerStmts[num] = {{start, end}, directly_nested_containerStmt};
-    return;
-}
-
