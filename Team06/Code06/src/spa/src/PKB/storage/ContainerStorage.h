ac3bafeff0a16ee6e68ccfe4ddfb20c0c2190a79
Darren <darrenhoon.ad@gmail.com>
2023-02-12 13:38:06 +0800

    changed container storage

--- Team06/Code06/src/spa/src/PKB/storage/ContainerStorage.h
+++ Team06/Code06/src/spa/src/PKB/storage/ContainerStorage.h
@@ -15,13 +15,22 @@ public:
     
 private:
     /*
-     * Structure of table example below
+     * Structure of table example below.
+     * First vector of range_nestedContainerStmts is range
+     * Second vector of range_nestedContainerStmts contains the directly nested StmtNums
      *  stmt | range_nestedContainerStmts
-     *  11 | [[12, 30], [15, 19]]
-     *  15 | [[16, 22], []]
-     *  19 | [[20, 26], [22]]
-     *  22 | [[23, 26], []]
+     *  11 | [12, 30] ie range of this container is from lines 12 rto 30. There is an if/while at line 15 and line 19
+     *  15 | [16, 22]
+     *  19 | [20, 26]
+     *  22 | [23, 26]
+     *
+     *
+     *  stmt | stmtNum_containerStmtNum
+     *  11 | [["if", 15], ["while", 19]]
+     *  15 | []
+     *  19 | [["while", 22]]
+     *  22 | []
      */
-    std::unordered_map<StmtNum, std::vector<std::vector<StmtNum>>> stmtNum_range_nestedContainerStmts;
-    std::unordered_map<StmtNum, std::vector<std::pair<std::string, StmtNum>>> stmtNum_containerStmtNum;
+    std::unordered_map<StmtNum, std::vector<std::vector<StmtNum>>> stmtNum_range;
+    std::unordered_map<StmtNum, std::vector<std::pair<std::string, StmtNum>>> stmtNum_directContainers;
 };
