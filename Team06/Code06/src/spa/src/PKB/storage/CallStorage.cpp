019ab6a2859448c905266754079b9f248d44cb55
Darren <darrenhoon.ad@gmail.com>
2023-02-12 13:56:35 +0800

    update CallStorage

--- Team06/Code06/src/spa/src/PKB/storage/CallStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/CallStorage.cpp
@@ -1,5 +1,10 @@
 #include "CallStorage.h"
 
-void CallStorage::writeCall(Stmt caller, Stmt callee) {
-    caller_callee[caller].insert(callee);
+//void CallStorage::writeCall(Stmt caller, Stmt callee) {
+//    caller_callee[caller].insert(callee);
+//}
+
+void CallStorage::writeCall(StmtNum callLine, Stmt callee) {
+    callLine_callee[callLine]= callee;
+    return;
 }
--- Team06/Code06/src/spa/src/PKB/storage/CallStorage.h
+++ Team06/Code06/src/spa/src/PKB/storage/CallStorage.h
@@ -11,7 +11,8 @@ typedef int StmtNum;
 
 class CallStorage {
 public:
-    virtual void writeCall(Stmt caller, Stmt callee);
+//    virtual void writeCall(Stmt caller, Stmt callee);
+    virtual void writeCall(StmtNum callLine, Stmt callee);
     
 private:
     /*
@@ -20,5 +21,15 @@ private:
      *  "proc1" | {"proc2", "proc3"}
      *  "proc2" | {"proc3"}
      */
-    std::unordered_map<Stmt, std::unordered_set<Stmt>> caller_callee;
+//    std::unordered_map<Stmt, std::unordered_set<Stmt>> caller_callee;
+
+
+
+    /*
+     * Structure of table example below
+     *  call | callee
+     *  11 | "proc2"
+     *  28 | "proc3"
+     */
+    std::unordered_map<StmtNum, std::string> callLine_callee;
 };
