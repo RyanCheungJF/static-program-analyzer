6094547075827ba0057b51bad5aaf1bad1a747f4
calvintanwj <calvintanwj127@gmail.com>
2023-02-16 15:51:19 +0800

    Remove dead method for Token

--- Team06/Code06/src/spa/src/SP/Token.cpp
+++ Team06/Code06/src/spa/src/SP/Token.cpp
@@ -5,10 +5,6 @@ Token::Token(TokenType type, std::string value) {
 	this->value = value;
 }
 
-std::string Token::toString() {
-	return this->value;
-}
-
 bool operator==(const Token& lhs, const Token& rhs) {
 	return lhs.type == rhs.type && lhs.value == rhs.value;
 }
