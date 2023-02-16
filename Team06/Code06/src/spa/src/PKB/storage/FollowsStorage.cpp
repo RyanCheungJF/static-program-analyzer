f7bd7a435868dde59aaacdb3cac113e64c6e96c6
Moley456 <kingstarwei10@gmail.com>
2023-02-16 12:50:37 +0800

    change Follows Storage to inherit from super class

--- Team06/Code06/src/spa/src/PKB/storage/FollowsStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/FollowsStorage.cpp
@@ -14,51 +14,6 @@ void FollowsStorage::write(StmtNum followee, StmtNum follower) {
      */
 
     // assume that SP gives us non-duplicates
-    followeeFollowerMap.insert({ followee, follower});
-    followerFolloweeMap.insert({ follower, followee});
-    return;
-}
-
-bool FollowsStorage::exists(StmtNum followee, StmtNum follower) {
-
-    // followee does not exist in table
-    if (followeeFollowerMap.find(followee) == followeeFollowerMap.end()) {
-        return false;
-    }
-
-    // follower does not exist in table
-    if (followerFolloweeMap.find(follower) == followerFolloweeMap.end()) {
-        return false;
-    }
-
-
-    return followerFolloweeMap.at(follower) == followee && followeeFollowerMap.at(followee) == follower;
-}
-
-std::unordered_set<StmtNum> FollowsStorage::getRightWildcard(StmtNum followee) {
-
-    // followee does not exist in table
-    if (followeeFollowerMap.find(followee) == followeeFollowerMap.end()) {
-        return std::unordered_set<StmtNum>();
-    }
-    return std::unordered_set<StmtNum> {followeeFollowerMap.at(followee)};
-}
-
-std::unordered_set<StmtNum> FollowsStorage::getLeftWildcard(StmtNum follower) {
-
-    // follower does not exist in table
-    if (followerFolloweeMap.find(follower) == followerFolloweeMap.end()) {
-        return std::unordered_set<StmtNum>();
-    }
-    return std::unordered_set<StmtNum> {followerFolloweeMap.at(follower)};
-}
-
-std::pair<std::vector<StmtNum>, std::vector<StmtNum>> FollowsStorage::getAllPairs() {
-    std::vector<StmtNum> followees;
-    std::vector<StmtNum> followers;
-    for (auto followeeFollower : followeeFollowerMap) {
-        followees.push_back(followeeFollower.first);
-        followers.push_back(followeeFollower.second);
-    }
-    return { followees, followers };
+    leftToRightMap.insert({ followee, {follower} });
+    rightToLeftMap.insert({ follower, {followee} });
 }
