b57976e9e2b8c68c546e2dd8e8e8099be64f6d48
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-02-16 22:57:56 +0800

    test: add test cases for QPSParser

--- Team06/Code06/src/unit_testing/src/QPS/TestQPSParser.cpp
+++ Team06/Code06/src/unit_testing/src/QPS/TestQPSParser.cpp
@@ -36,3 +36,86 @@ TEST_CASE("splitQuery / splitting variable v; Select v; should give error / catc
     CHECK_THROWS(qp.splitQuery(test));
 }
 
+TEST_CASE("checkSynonyms / the variable store contains all the required variables in query / no exceptions thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Parameter p1("v", "variable");
+    Parameter p2("s", "stmt");
+    vs.insertVariable(p1);
+    vs.insertVariable(p2);
+    Query q = sqp.parse("Select s such that Modifies(s, v)");
+    REQUIRE_NOTHROW(qp.checkSynonyms(&q, vs));
+}
+
+TEST_CASE("checkSynonyms / the variable store does not contain stmt / exception is thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Parameter p1("v", "variable");
+    vs.insertVariable(p1);
+    Query q = sqp.parse("Select s such that Modifies(s, v)");
+    REQUIRE_THROWS(qp.checkSynonyms(&q, vs));
+}
+
+TEST_CASE("checkSynonyms / the variable store does not contain variable / exception is thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Parameter p1("s", "stmt");
+    vs.insertVariable(p1);
+    Query q = sqp.parse("Select s such that Modifies(s, v)");
+    REQUIRE_THROWS(qp.checkSynonyms(&q, vs));
+}
+
+TEST_CASE("checkSynonyms / variable store is empty / exception is thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Query q = sqp.parse("Select s");
+    REQUIRE_THROWS(qp.checkSynonyms(&q, vs));
+}
+
+TEST_CASE("checkSynonyms / the variable store contains more variable than needed / no exception is thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Parameter p1("s", "stmt");
+    Parameter p2("s2", "stmt");
+    Parameter p3("s3", "stmt");
+    Parameter p4("s4", "stmt");
+    Parameter p5("v", "variable");
+    Parameter p6("v2", "variable");
+    Parameter p7("v3", "variable");
+    Parameter p8("v4", "variable");
+    vs.insertVariable(p1);
+    vs.insertVariable(p2);
+    vs.insertVariable(p3);
+    vs.insertVariable(p4);
+    vs.insertVariable(p5);
+    vs.insertVariable(p6);
+    vs.insertVariable(p7);
+    vs.insertVariable(p8);
+    Query q = sqp.parse("Select s such that Modifies(s, v)");
+    REQUIRE_NOTHROW(qp.checkSynonyms(&q, vs));
+}
+
+TEST_CASE("checkSynonyms / the variable store has correct type but wrong synonym / exception is thrown") {
+    QPSParser qp;
+    VariableStore vs;
+    SelectQueryParser sqp;
+    Parameter p2("s2", "stmt");
+    Parameter p3("s3", "stmt");
+    Parameter p4("s4", "stmt");
+    Parameter p6("v2", "variable");
+    Parameter p7("v3", "variable");
+    Parameter p8("v4", "variable");
+    vs.insertVariable(p2);
+    vs.insertVariable(p3);
+    vs.insertVariable(p4);
+    vs.insertVariable(p6);
+    vs.insertVariable(p7);
+    vs.insertVariable(p8);
+    Query q = sqp.parse("Select s such that Modifies(s, v)");
+    REQUIRE_THROWS(qp.checkSynonyms(&q, vs));
+}
