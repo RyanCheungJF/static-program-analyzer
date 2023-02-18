d12c71f900581049119651222db777568248d871
calvintanwj <calvintanwj127@gmail.com>
2023-02-18 22:24:33 +0800

    Refactor Tokenizer

--- Team06/Code06/src/spa/src/SP/Token.h
+++ Team06/Code06/src/spa/src/SP/Token.h
@@ -9,6 +9,8 @@ public:
 	std::string value;
 
 	Token(TokenType type, std::string value);
+	Token(TokenType type);
+
 	bool isType(TokenType tt);
 	bool hasValue(std::string val);
 	friend bool operator==(const Token& lhs, const Token& rhs);
