cd34903736465df3b49af35e930aac6e7b007905
Moley456 <kingstarwei10@gmail.com>
2023-03-02 02:05:00 +0800

    write if/while pattern storage unit tests

--- Team06/Code06/src/unit_testing/src/PKB/TestPattern.cpp
+++ Team06/Code06/src/unit_testing/src/PKB/TestPattern.cpp
@@ -5,3 +5,69 @@
 #include "catch.hpp"
 
 using namespace unit_testing_utils;
+using namespace std;
+
+TEST_CASE("Check Pattern Storage Write correctness") {
+  PatternStorage ps;
+  ps.writePattern(1, {"x", "y"});
+
+  SECTION("Check Writes do not override existing values") {
+    ps.writePattern(2, {"y"});
+
+    unordered_set<StmtNum> res = ps.getStmtNums("y");
+
+    unordered_set<StmtNum> expected = {1, 2};
+    REQUIRE(res.size() == 2);
+    REQUIRE(equals(res, expected));
+  }
+  
+  SECTION("Check that writing existing values do not cause duplicates") {
+    ps.writePattern(1, {"x", "y"});
+
+    unordered_set<StmtNum> res = ps.getStmtNums("y");
+
+    unordered_set<StmtNum> expected = {1};
+    REQUIRE(res.size() == 1);
+    REQUIRE(equals(res, expected));
+  }
+}
+
+TEST_CASE("Check Pattern Storage Write and non-empty Gets") {
+  PatternStorage ps;
+  ps.writePattern(1, {"x", "y"});
+  ps.writePattern(2, {"y", "z"});
+  ps.writePattern(3, {"z"});
+
+  SECTION("Get statement numbers of existing pattern") {
+    unordered_set<StmtNum> res = ps.getStmtNums("y");
+    unordered_set<StmtNum> expected = {1, 2};
+    REQUIRE(res.size() == 2);
+    REQUIRE(equals(res, expected));
+  }
+
+  SECTION("Get all statement number - variable pairs") {
+    pair<vector<StmtNum>, vector<Ent>> res = ps.getAllPairs();
+    vector<StmtNum> stmtNums = res.first;
+    vector<Ent> vars = res.second;
+
+    vector<StmtNum> expectedStmtNums = {1, 1, 2, 2, 3};
+    REQUIRE(stmtNums.size() == 5);
+    REQUIRE(equals(stmtNums, expectedStmtNums));
+
+    vector<Ent> expectedVars = {"x", "y", "y", "z", "z"};
+    REQUIRE(res.second.size() == 5);
+    REQUIRE(equals(vars, expectedVars));
+  }
+}
+
+TEST_CASE("Check Pattern Storage Write and empty Gets") {
+  PatternStorage ps;
+  ps.writePattern(1, {"x", "y"});
+  ps.writePattern(2, {"y", "z"});
+  ps.writePattern(3, {"z"});
+
+  SECTION("Get statement numbers of non-existing pattern") {
+    unordered_set<StmtNum> res = ps.getStmtNums("non-existing");
+    REQUIRE(res.size() == 0);
+  }
+}
--- Team06/Code06/src/unit_testing/src/utils/utils.cpp
+++ Team06/Code06/src/unit_testing/src/utils/utils.cpp
@@ -1,44 +1,50 @@
 #include "utils.h"
 
 bool unit_testing_utils::contains(std::vector<std::vector<std::string>> result, std::vector<std::string> item) {
-	return find(result.begin(), result.end(), item) != result.end();
+  return find(result.begin(), result.end(), item) != result.end();
+}
+
+bool unit_testing_utils::equals(std::vector<std::string> expected, std::vector<std::string> actual) {
+  std::sort(actual.begin(), actual.end());
+  std::sort(expected.begin(), expected.end());
+  return expected == actual;
 }
 
 bool unit_testing_utils::equals(std::vector<int> expected, std::vector<int> actual) {
-    std::sort(actual.begin(), actual.end());
-    std::sort(expected.begin(), expected.end());
-    return expected == actual;
+  std::sort(actual.begin(), actual.end());
+  std::sort(expected.begin(), expected.end());
+  return expected == actual;
 }
 
 bool unit_testing_utils::equals(std::vector<int> expected, std::unordered_set<int> res) {
-    std::vector<int> actual;
-    for (auto i : res) {
-        actual.push_back(i);
-    }
-    std::sort(actual.begin(), actual.end());
-    std::sort(expected.begin(), expected.end());
-    return expected == actual;
+  std::vector<int> actual;
+  for (auto i : res) {
+    actual.push_back(i);
+  }
+  std::sort(actual.begin(), actual.end());
+  std::sort(expected.begin(), expected.end());
+  return expected == actual;
 }
 
 bool unit_testing_utils::equals(std::unordered_set<int> param1, std::unordered_set<int> param2) {
-    std::vector<int> actual;
-    for (auto i : param2) {
-        actual.push_back(i);
-    }
+  std::vector<int> actual;
+  for (auto i : param2) {
+    actual.push_back(i);
+  }
 
-    std::vector<int> expected;
-    for (auto i : param1) {
-        expected.push_back(i);
-    }
+  std::vector<int> expected;
+  for (auto i : param1) {
+    expected.push_back(i);
+  }
 
-    std::sort(actual.begin(), actual.end());
-    std::sort(expected.begin(), expected.end());
-    return expected == actual;
+  std::sort(actual.begin(), actual.end());
+  std::sort(expected.begin(), expected.end());
+  return expected == actual;
 }
 
 bool unit_testing_utils::equals(std::vector<std::vector<std::string>> expected,
                                 std::vector<std::vector<std::string>> actual) {
-    std::sort(actual.begin(), actual.end());
-    std::sort(expected.begin(), expected.end());
-    return expected == actual;
+  std::sort(actual.begin(), actual.end());
+  std::sort(expected.begin(), expected.end());
+  return expected == actual;
 }
