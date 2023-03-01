6a3ff6c0fdfe3c70eef22a95e61b9f3c67e1b335
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-03-01 13:50:04 +0800

    feat: add parsing for while and ifs pattern clause

---
+++ Team06/Code06/src/spa/src/qps/entities/WhilePattern.h
@@ -0,0 +1,13 @@
+#ifndef SPA_WHILEPATTERN_H
+#define SPA_WHILEPATTERN_H
+#include "Pattern.h"
+
+
+class WhilePattern : public Pattern {
+public:
+    WhilePattern(Parameter ps, Parameter er, string p);
+    bool validateParams();
+};
+
+
+#endif //SPA_WHILEPATTERN_H
