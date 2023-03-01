f2411ac78adb177d2b6b0c40ee1071a852b6958b
Moley456 <kingstarwei10@gmail.com>
2023-03-01 21:59:05 +0800

    add if/while pattern unit tests

---
+++ Team06/Code06/src/unit_testing/src/PKB/TestPattern.cpp
@@ -0,0 +1,7 @@
+#include "../../../spa/src/PKB/ReadPKB.h"
+#include "../../../spa/src/PKB/WritePKB.h"
+#include "../../../spa/src/PKB/utils/utils.h"
+#include "../utils/utils.h"
+#include "catch.hpp"
+
+using namespace unit_testing_utils;
