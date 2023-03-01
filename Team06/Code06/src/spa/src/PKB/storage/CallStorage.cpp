59115e0a203222210ee0436f0c0b6f2f31f1fd8a
Darren <darrenhoon.ad@gmail.com>
2023-03-01 19:43:45 +0800

    use the clang formatter on storages

--- Team06/Code06/src/spa/src/PKB/storage/CallStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/CallStorage.cpp
@@ -1,22 +1,22 @@
 #include "CallStorage.h"
 
 void CallStorage::writeCall(StmtNum callLine, ProcName callee) {
-    callLine_callee[callLine]= callee;
+  callLine_callee[callLine] = callee;
 }
 
 std::vector<std::pair<StmtNum, ProcName>> CallStorage::getCallStatements() {
-    std::vector<std::pair<StmtNum, ProcName>> res;
-    for (auto i : callLine_callee) {
-        res.push_back(std::make_pair(i.first, i.second));
-    }
-    return res;
+  std::vector<std::pair<StmtNum, ProcName>> res;
+  for (auto i : callLine_callee) {
+    res.push_back(std::make_pair(i.first, i.second));
+  }
+  return res;
 }
 
 std::pair<StmtNum, ProcName> CallStorage::getCallStmt(StmtNum s) {
-    // SHOULD NOT HIT.
-    if (callLine_callee.find(s) == callLine_callee.end()) {
-        std::pair<StmtNum, ProcName> emptyPair = std::make_pair<StmtNum, ProcName>(-1, "INVALID");
-        return emptyPair;
-    }
-    return std::make_pair(s, callLine_callee[s]);
+  // SHOULD NOT HIT.
+  if (callLine_callee.find(s) == callLine_callee.end()) {
+    std::pair<StmtNum, ProcName> emptyPair = std::make_pair<StmtNum, ProcName>(-1, "INVALID");
+    return emptyPair;
+  }
+  return std::make_pair(s, callLine_callee[s]);
 }
--- Team06/Code06/src/spa/src/PKB/storage/FollowsParentStorage.cpp
+++ Team06/Code06/src/spa/src/PKB/storage/FollowsParentStorage.cpp
@@ -1,51 +1,49 @@
 #include "FollowsParentStorage.h"
 
 void FollowsParentStorage::write(StmtNum leftNum, StmtNum rightNum) {
-    leftToRightMap[leftNum].insert(rightNum);
-    rightToLeftMap[rightNum].insert(leftNum);
+  leftToRightMap[leftNum].insert(rightNum);
+  rightToLeftMap[rightNum].insert(leftNum);
 }
 
 void FollowsParentStorage::write(StmtNum leftNum, std::unordered_set<StmtNum> rightNums) {
-    leftToRightMap[leftNum].insert(rightNums.begin(), rightNums.end());
-    for (StmtNum rightNum : rightNums) {
-        rightToLeftMap[rightNum].insert(leftNum);
-    }
+  leftToRightMap[leftNum].insert(rightNums.begin(), rightNums.end());
+  for (StmtNum rightNum : rightNums) {
+    rightToLeftMap[rightNum].insert(leftNum);
+  }
 }
 
 bool FollowsParentStorage::exists(StmtNum leftNum, StmtNum rightNum) {
-    if (leftToRightMap.find(leftNum) == leftToRightMap.end()) {
-        return false;
-    }
-    return leftToRightMap[leftNum].find(rightNum) != leftToRightMap[leftNum].end();
+  if (leftToRightMap.find(leftNum) == leftToRightMap.end()) {
+    return false;
+  }
+  return leftToRightMap[leftNum].find(rightNum) != leftToRightMap[leftNum].end();
 }
 
 std::unordered_set<StmtNum> FollowsParentStorage::getRightWildcard(StmtNum leftNum) {
-    if (leftToRightMap.find(leftNum) == leftToRightMap.end()) {
-        std::unordered_set<StmtNum> emptySet;
-        return emptySet;
-    }
-    return leftToRightMap[leftNum];
-
+  if (leftToRightMap.find(leftNum) == leftToRightMap.end()) {
+    std::unordered_set<StmtNum> emptySet;
+    return emptySet;
+  }
+  return leftToRightMap[leftNum];
 }
 
 std::unordered_set<StmtNum> FollowsParentStorage::getLeftWildcard(StmtNum rightNum) {
-    if (rightToLeftMap.find(rightNum) == rightToLeftMap.end()) {
-        std::unordered_set<StmtNum> emptySet;
-        return emptySet;
-    }
-    return rightToLeftMap[rightNum];
+  if (rightToLeftMap.find(rightNum) == rightToLeftMap.end()) {
+    std::unordered_set<StmtNum> emptySet;
+    return emptySet;
+  }
+  return rightToLeftMap[rightNum];
 }
 
 std::pair<std::vector<StmtNum>, std::vector<StmtNum>> FollowsParentStorage::getAllPairs() {
-    std::vector<StmtNum> allLeftNums;
-    std::vector<StmtNum> allRightNums;
-    for (auto row : leftToRightMap) {
-        StmtNum leftNum = row.first;
-        for (auto rightNum : row.second) {
-            allLeftNums.push_back(leftNum);
-            allRightNums.push_back(rightNum);
-        }
-
+  std::vector<StmtNum> allLeftNums;
+  std::vector<StmtNum> allRightNums;
+  for (auto row : leftToRightMap) {
+    StmtNum leftNum = row.first;
+    for (auto rightNum : row.second) {
+      allLeftNums.push_back(leftNum);
+      allRightNums.push_back(rightNum);
     }
-    return { allLeftNums, allRightNums };
+  }
+  return {allLeftNums, allRightNums};
 }
