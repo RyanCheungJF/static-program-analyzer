08f2bbcd40f1a268390d99b5d3103f81d820cf24
Moley456 <kingstarwei10@gmail.com>
2023-03-01 23:20:39 +0800

    style format pkb files

--- Team06/Code06/src/spa/src/PKB/WritePKB.h
+++ Team06/Code06/src/spa/src/PKB/WritePKB.h
@@ -4,52 +4,52 @@
 
 class WritePKB {
 public:
-  // Sets the pointer to the PKB instance if it is not set yet
-  void setInstancePKB(PKB &pkb);
+    // Sets the pointer to the PKB instance if it is not set yet
+    void setInstancePKB(PKB& pkb);
 
-  // Sets Follows relation in PKB
-  void setFollows(StmtNum left, StmtNum right);
+    // Sets Follows relation in PKB
+    void setFollows(StmtNum left, StmtNum right);
 
-  // Sets FollowsT relation in PKB
-  void setFollowsT(StmtNum followee, std::unordered_set<StmtNum> followers);
+    // Sets FollowsT relation in PKB
+    void setFollowsT(StmtNum followee, std::unordered_set<StmtNum> followers);
 
-  // Sets Parent relation in PKB
-  void setParent(StmtNum parent, StmtNum child);
+    // Sets Parent relation in PKB
+    void setParent(StmtNum parent, StmtNum child);
 
-  // Sets ParentT relation in PKB
-  void setParentT(StmtNum parent, std::unordered_set<StmtNum> children);
+    // Sets ParentT relation in PKB
+    void setParentT(StmtNum parent, std::unordered_set<StmtNum> children);
 
-  // Sets the statement along with the statement line that it appears in
-  void setStatement(Stmt s, StmtNum line);
+    // Sets the statement along with the statement line that it appears in
+    void setStatement(Stmt s, StmtNum line);
 
-  // Sets the entity along with the statement line that the entities appears in
-  void setEntity(StmtNum line, std::unordered_set<Ent> entities);
+    // Sets the entity along with the statement line that the entities appears in
+    void setEntity(StmtNum line, std::unordered_set<Ent> entities);
 
-  // Sets the entity along with the statement line that it appears in
-  void setEntity(Ent e, StmtNum line);
+    // Sets the entity along with the statement line that it appears in
+    void setEntity(Ent e, StmtNum line);
 
-  // Sets the procedure along with the statement lines that are in that
-  // procedure appears in
-  void setProcedure(ProcName p, std::unordered_set<StmtNum> lines);
+    // Sets the procedure along with the statement lines that are in that
+    // procedure appears in
+    void setProcedure(ProcName p, std::unordered_set<StmtNum> lines);
 
-  // Sets the constants along with the statement line that the constants appears
-  // in
-  void setConstant(StmtNum num, std::unordered_set<Const> constants);
+    // Sets the constants along with the statement line that the constants appears
+    // in
+    void setConstant(StmtNum num, std::unordered_set<Const> constants);
 
-  void setCall(StmtNum callLine, ProcName procedure_being_called);
+    void setCall(StmtNum callLine, ProcName procedure_being_called);
 
-  void setUsesS(StmtNum num, std::unordered_set<Ent> entities);
+    void setUsesS(StmtNum num, std::unordered_set<Ent> entities);
 
-  void setUsesP(ProcName, std::unordered_set<Ent> entities);
+    void setUsesP(ProcName, std::unordered_set<Ent> entities);
 
-  void setModifiesS(StmtNum num, std::unordered_set<Ent> entities);
+    void setModifiesS(StmtNum num, std::unordered_set<Ent> entities);
 
-  void setModifiesP(ProcName, std::unordered_set<Ent> entities);
+    void setModifiesP(ProcName, std::unordered_set<Ent> entities);
 
-  void writePattern(std::string lhs, StmtNum num, std::unique_ptr<Expression> pointer);
+    void writePattern(std::string lhs, StmtNum num, std::unique_ptr<Expression> pointer);
 
-  void writeCFG(StmtNum num, CFGNodeStub &root);
+    void writeCFG(StmtNum num, CFGNodeStub& root);
 
 private:
-  PKB *pkbInstance = NULL;
+    PKB* pkbInstance = NULL;
 };
--- Team06/Code06/src/unit_testing/src/utils/utils.h
+++ Team06/Code06/src/unit_testing/src/utils/utils.h
@@ -4,10 +4,10 @@
 #include <vector>
 
 namespace unit_testing_utils {
-  bool contains(std::vector<std::vector<std::string>> result, std::vector<std::string> item);
-  bool equals(std::vector<std::string> expected, std::vector<std::string> actual);
-  bool equals(std::vector<int> expected, std::vector<int> actual);
-  bool equals(std::vector<int> expected, std::unordered_set<int> actual);
-  bool equals(std::unordered_set<int> expected, std::unordered_set<int> actual);
-  bool equals(std::vector<std::vector<std::string>> expected, std::vector<std::vector<std::string>> actual);
+    bool contains(std::vector<std::vector<std::string>> result, std::vector<std::string> item);
+    bool equals(std::vector<std::string> expected, std::vector<std::string> actual);
+    bool equals(std::vector<int> expected, std::vector<int> actual);
+    bool equals(std::vector<int> expected, std::unordered_set<int> actual);
+    bool equals(std::unordered_set<int> expected, std::unordered_set<int> actual);
+    bool equals(std::vector<std::vector<std::string>> expected, std::vector<std::vector<std::string>> actual);
 }
