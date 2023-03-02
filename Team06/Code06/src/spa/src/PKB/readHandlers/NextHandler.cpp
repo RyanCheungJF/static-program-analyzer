7d65c1dc38145127132d55709ee4eff5ac0239a9
Darren <darrenhoon.ad@gmail.com>
2023-03-02 16:25:17 +0800

    Add Next cases

--- Team06/Code06/src/spa/src/PKB/readHandlers/NextHandler.cpp
+++ Team06/Code06/src/spa/src/PKB/readHandlers/NextHandler.cpp
@@ -5,3 +5,32 @@ NextHandler::NextHandler(std::shared_ptr<CFGStorage> cfgStorage, std::shared_ptr
     this->stmtStorage = stmtStorage;
 }
 
+std::vector<std::vector<std::string>> NextHandler::handle(Parameter param1, Parameter param2) {
+
+    std::string paramString1 = param1.getValue();
+    std::string paramString2 = param2.getValue();
+    ParameterType paramType1 = param1.getType();
+    ParameterType paramType2 = param2.getType();
+
+    if (paramType1 == ParameterType::FIXED_INT && paramType2 == ParameterType::FIXED_INT) {
+        return handleIntInt(param1, param2);
+    } else if (paramType1 == ParameterType::FIXED_INT && paramType2 == ParameterType::WILDCARD) {
+        return handleIntWildcard(param1);
+    } else if (paramType1 == ParameterType::WILDCARD && paramType2 == ParameterType::FIXED_INT) {
+        return handleWildcardInt(param2);
+    } else if ((stmtTypesSet.find(paramType1) != stmtTypesSet.end()) && paramType2 == ParameterType::FIXED_INT) {
+        return handleStmttypeInt(param1, param2);
+    } else if (paramType1 == ParameterType::FIXED_INT && (stmtTypesSet.find(paramType2) != stmtTypesSet.end())) {
+        return handleIntStmttype(param1, param2);
+    } else if ((stmtTypesSet.find(paramType1) != stmtTypesSet.end()) && paramType2 == ParameterType::WILDCARD) {
+        return handleStmttypeWildcard(param1);
+    } else if (paramType1 == ParameterType::WILDCARD && (stmtTypesSet.find(paramType2) != stmtTypesSet.end())) {
+        return handleWildcardStmttype(param2);
+    } else if ((stmtTypesSet.find(paramType1) != stmtTypesSet.end()) && (stmtTypesSet.find(paramType2) != stmtTypesSet.end())) {
+        return handleStmttypeStmttype(param1, param2);
+    } else if (paramType1 == ParameterType::WILDCARD && paramType2 == ParameterType::WILDCARD) {
+        return handleWildcardWildcard();
+    }
+    return std::vector<std::vector<std::string>>();
+}
+
--- Team06/Code06/src/spa/src/PKB/readHandlers/NextHandler.h
+++ Team06/Code06/src/spa/src/PKB/readHandlers/NextHandler.h
@@ -15,25 +15,35 @@ private:
     std::shared_ptr<StmtStorage> stmtStorage;
 
     // e.g. Next(1, 2)
+    std::vector<std::vector<std::string>> handleIntInt(Parameter param1, Parameter param2);
+
     // e.g. Next(1, s2) or Next(1, _)
+    std::vector<std::vector<std::string>> handleIntWildcard(Parameter param2);
+
     // e.g. Next(s1, 5) or Next(_, 5)
+    std::vector<std::vector<std::string>> handleWildcardInt(Parameter param2);
+
     // e.g. Next(a1, 5) where a1 is assign. a1 can be replaced by other valid statement types
+    std::vector<std::vector<std::string>> handleStmttypeInt(Parameter param1, Parameter param2);
+
     // e.g. Next(5, a1) where a1 is assign. a1 can be replaced by other valid statement types
+    std::vector<std::vector<std::string>> handleIntStmttype(Parameter param1, Parameter param2);
+
+    // e.g. Next(a1, _) where a1 is assign. a1 can be replaced by other valid statement types
+    std::vector<std::vector<std::string>> handleStmttypeWildcard(Parameter param1);
+
+    // e.g. Next(_, a1) where a1 is assign. a1 can be replaced by other valid statement types
+    std::vector<std::vector<std::string>> handleWildcardStmttype(Parameter param2);
+
     // e.g. Next(w1, a1) where a1 is assign, w1 is while. They can be replaced by other valid statement types
+    std::vector<std::vector<std::string>> handleStmttypeStmttype(Parameter param1, Parameter param2);
 
-//    std::vector<std::vector<std::string>> handleProcnameProcname(Parameter param1, Parameter param2);
-//
-//    // e.g. Next(1, s2) or Calls(1, _)
-//    std::vector<std::vector<std::string>> handleProcnameWildcard(Parameter param1, Parameter param2);
-//
-//    // e.g. Next(1, s2) or Calls(1, _)
-//    std::vector<std::vector<std::string>> handleProcnameWildcard(Parameter param1, Parameter param2);
-//
-//    // e.g. Calls(_, "proc2") or Calls(p1, "proc2")
-//    std::vector<std::vector<std::string>> handleWildcardProcname(Parameter param1, Parameter param2);
-//
-//    // e.g. Calls(_, _)
-//    std::vector<std::vector<std::string>> handleWildcardWildcard();
+    // e.g. Next(s1, s2) or Next(_, _)
+    std::vector<std::vector<std::string>> handleWildcardWildcard();
 
+
+private:
+    std::unordered_set<ParameterType> stmtTypesSet = {ParameterType::IF, ParameterType::WHILE, ParameterType::ASSIGN,
+                                                   ParameterType::PRINT, ParameterType::READ, ParameterType::CALL};
 };
 
