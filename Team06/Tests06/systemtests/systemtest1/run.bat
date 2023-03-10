9933585e3adc6b86d73a9f65cb47197e5dff6bd8
Ryan Cheung <ryan9cheung@gmail.com>
2023-03-10 17:52:08 +0800

    Add sample tests for tt, Affects

--- Team06/Tests06/systemtests/systemtest1/run.bat
+++ Team06/Tests06/systemtests/systemtest1/run.bat
@@ -23,3 +23,6 @@
 
 ..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt Next.txt out-Next.xml
 ..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt NextT.txt out-NextT.xml
+
+..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt Affects.txt out-Affects.xml
+..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt AffectsT.txt out-AffectsT.xml
