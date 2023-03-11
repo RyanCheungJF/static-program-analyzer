c1740db507f24287824dbbc9a15c5f1fa477be95
Ryan Cheung <ryan9cheung@gmail.com>
2023-03-11 16:46:26 +0800

    Add Next and NextT for sys3

--- Team06/Tests06/systemtests/systemtest3/run.bat
+++ Team06/Tests06/systemtests/systemtest3/run.bat
@@ -1,2 +1,5 @@
 ..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt Calls.txt out-Calls.xml
 ..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt CallsT.txt out-CallsT.xml
+
+..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt Next.txt out-Next.xml
+..\..\..\Code06\out\build\x64-Debug\src\autotester\autotester.exe source.txt NextT.txt out-NextT.xml
