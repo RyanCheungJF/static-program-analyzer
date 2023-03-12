0c611b7885668f1574172821167288ae3bcb5b71
Darren <darrenhoon.ad@gmail.com>
2023-03-12 19:34:50 +0800

    add Strategy parent class

--- Team06/Code06/src/spa/src/PKB/readHandlers/Strategies/Strategy.cpp
+++ Team06/Code06/src/spa/src/PKB/readHandlers/Strategies/Strategy.cpp
@@ -1,5 +1,2 @@
-//
-// Created by admin on 12/3/23.
-//
-
 #include "Strategy.h"
+
--- Team06/Code06/src/spa/src/PKB/readHandlers/Strategies/Strategy.h
+++ Team06/Code06/src/spa/src/PKB/readHandlers/Strategies/Strategy.h
@@ -1,16 +1,5 @@
-//
-// Created by admin on 12/3/23.
-//
-
-#ifndef SPA_STRATEGY_H
-#define SPA_STRATEGY_H
-
 
 
 class Strategy {
 
 };
-
-
-
-#endif //SPA_STRATEGY_H
