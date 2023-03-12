2df3b5e0691f1c33288d683df23c97f69373039d
Ryan Cheung <ryan9cheung@gmail.com>
2023-03-12 16:35:16 +0800

    Add remaining sys test files

--- Team06/Tests06/systemtests/systemtest3/run.sh
+++ Team06/Tests06/systemtests/systemtest3/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#../../../Code06/cmake-build-debug/src/autotester/autotester source.txt MultiClause.txt out-MultiClause.xml
+
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Calls.txt out-Calls.xml
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt CallsT.txt out-CallsT.xml
 
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Next.txt out-Next.xml
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt NextT.txt out-NextT.xml
+
+#../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Affects.txt out-Affects.xml
+#../../../Code06/cmake-build-debug/src/autotester/autotester source.txt AffectsT.txt out-AffectsT.xml
+
+#../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Boolean.txt out-Boolean.xml
+
+#../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Tuple.txt out-Tuple.xml
