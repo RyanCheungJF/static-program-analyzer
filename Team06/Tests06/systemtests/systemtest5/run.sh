f3311bb6196d67e55929182ccf53ac44efb829ef
calvintanwj <calvintanwj127@gmail.com>
2023-03-15 23:43:23 +0800

    Add more calls/error system tests

--- Team06/Tests06/systemtests/systemtest5/run.sh
+++ Team06/Tests06/systemtests/systemtest5/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Calls.txt out-Calls.xml
+../../../Code06/cmake-build-debug/src/autotester/autotester source.txt CallsT.txt out-CallsT.xml
 ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt ErrorCheck.txt out-ErrorCheck.xml
