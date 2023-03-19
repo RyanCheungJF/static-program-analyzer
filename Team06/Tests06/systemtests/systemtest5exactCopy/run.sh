13f3ef2237e9c7dda2b1c253a5033e71758b5451
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-03-20 00:26:13 +0800

    remove systest5 duplicate

--- Team06/Tests06/systemtests/systemtest5exactCopy/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# ../../../Code06/cmake-build-debug/src/autotester/autotester source.txt Affects.txt out-Affects.xml
-../../../Code06/cmake-build-debug/src/autotester/autotester source.txt AffectsT.txt out-AffectsT.xml
