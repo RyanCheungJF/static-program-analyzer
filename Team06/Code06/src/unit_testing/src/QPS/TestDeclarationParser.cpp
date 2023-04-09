a50e838b2158e74552901c0eace97ac05a694b3c
Darren <darrenhoon.ad@gmail.com>
2023-04-09 16:31:48 +0800

    delete unused test case

--- Team06/Code06/src/unit_testing/src/QPS/TestDeclarationParser.cpp
+++ Team06/Code06/src/unit_testing/src/QPS/TestDeclarationParser.cpp
@@ -2,13 +2,6 @@
 #include "catch.hpp"
 using namespace std;
 
-TEST_CASE("parseDeclaration / stores two stmt declaration into store / store "
-          "contains 2 declarations of same type") {
-    vector<string> declarations = {"stmt s1, s2", "call cl", "assign a"};
-    VariableStore store = parseDeclarations(declarations);
-    // change this to an assertion
-}
-
 TEST_CASE("parseDeclaration / cannot store declaration with same name / throws "
           "Semantic exception") {
     vector<string> declarations = {"stmt s, s"};
